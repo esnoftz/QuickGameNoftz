45c4486c9ac0a2e6f32d8965280bfd7d8a17e39d
esnoftz <esnoftz@gmail.com>
2025-02-14 08:01:42 -0600

    Initial Commit

---
+++ QuickGameNoftzTests/QuickGameNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickGameNoftzTests.swift
+//  QuickGameNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 2/14/25.
+//
+
+import Testing
+@testable import QuickGameNoftz
+
+struct QuickGameNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
