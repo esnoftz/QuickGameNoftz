e8996005445c1bba4187a06fa2e06fcbcdc4a57a
esnoftz <esnoftz@gmail.com>
2025-02-20 08:09:17 -0600

    Worked on game.

--- QuickGameNoftz/GameScene.swift
+++ QuickGameNoftz/GameScene.swift
@@ -8,7 +8,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var ball: SKSpriteNode!
     let cam = SKCameraNode()
@@ -16,19 +16,34 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         self.camera = cam
-        //physicsWorld.contactDelegate = self
+        physicsWorld.contactDelegate = self
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         cam.position.x = ball.position.x + 300
-        //ball.physicsBody?.velocity.dx = 200
+        ball.physicsBody?.velocity.dx = 300
 
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
         print("Collision happened")
-        AppData.points += 1
+
+        if contact.bodyA.node?.name == "bob" || contact.bodyB.node?.name == "bob" {
+            AppData.points += 1
+            // removes bob when gru collides with him
+            if contact.bodyA.node?.name == "bob" {
+                contact.bodyA.node?.removeFromParent()
+            } else {
+                contact.bodyB.node?.removeFromParent()
+            }
+        } else if contact.bodyA.node?.name == "elMacho" || contact.bodyB.node?.name == "elMacho" {
+            AppData.points = 0
+        } else if contact.bodyA.node?.name == "scarletOverkill" || contact.bodyB.node?.name == "scarletOverkill" {
+            AppData.points = 0
+        }
+
     }
 }
